efc3c7fe2aca4c62af7c80b0ba9d55d7210f09eb
Poostoi <oleg.nour@bk.ru>
2023-04-04 22:15:08 +0300

    init

---
+++ Authorization.Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Authorization.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+        
+}
---
+++ Authorization.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+using Authorization.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authorization.Infrastructure;
+
+public class ApplicationDbContext:DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public ApplicationDbContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=test;Username=postgres;Password=postgres");
+    }
+}
